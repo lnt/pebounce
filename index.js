0140622c5d3611a6179a7914e811af2ffaccf60e
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-06-14 11:21:38 +0530

    version 0.0.2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 import debounce from 'debounce';
 
-function pebounce(callback,p1,p2,p3) {
+const pebounce = function pebounce(callback,p1,p2,p3) {
     let promise,resolve;
     var debounced = debounce(function(a1,a2,a3,a4) {
         resolve(callback(a1,a2,a3,a4)); 
         promise = null;  
     },p1,p2,p3);
-    pebounce  = function(a1,a2,a3,a4){
+    let pebounced  = function(a1,a2,a3,a4){
         promise =  promise || new Promise(function (r) {
             resolve = r;
         });
@@ -15,15 +15,15 @@ function pebounce(callback,p1,p2,p3) {
         return promise;
     }
 
-    pebounce.clear = function (argument) {
+    pebounced.clear = function (argument) {
         debounced.clear(argument)
     }
 
-    pebounce.flush = function (argument) {
+    pebounced.flush = function (argument) {
         debounced.flush(argument)
     }
     
-    return pebounce;
+    return pebounced;
 }
 
 
