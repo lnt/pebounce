aa446b4178453bb35d8293b7e525b9a53cc1a60b
Lalit tanwar <lalit.tanwar07@gmail.com>
2021-06-14 09:41:46 +0530

    added clear and flush funtion

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ function pebounce(callback,p1,p2,p3) {
         resolve(callback(a1,a2,a3,a4)); 
         promise = null;  
     },p1,p2,p3);
-    return function(a1,a2,a3,a4){
+    pebounce  = function(a1,a2,a3,a4){
         promise =  promise || new Promise(function (r) {
             resolve = r;
         });
         debounced(a1,a2,a3,a4);
         return promise;
     }
+
+    pebounce.clear = function (argument) {
+        debounced.clear(argument)
+    }
+
+    pebounce.flush = function (argument) {
+        debounced.flush(argument)
+    }
+    
+    return pebounce;
 }
 
 
